f63218b78931fd1d5d81188c1acf52c07dd749f3
Abu Suraj <abusurajbd@gmail.com>
2023-10-31 22:42:35 +0600

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+
 import './App.css'
 import Navbar from './page-sections/Navbar/Navbar';
 import HeroSection from './page-sections/Hero-section/HeroSection';
@@ -48,34 +48,4 @@ function App() {
 }
 
 export default App;
-
-
-
-// import About from './page-sections/About/About'
-// import Clients from './page-sections/Clients/Clients'
-// import Footer from './page-sections/Footer/Footer'
-// import HeroSection from './page-sections/Hero-section/HeroSection'
-// import Navbar from './page-sections/Navbar/Navbar'
-// import ServiceSection from './page-sections/Our-Services/Services'
-// import ProcessSection from './page-sections/Process-Section/ProcessSection'
-// import StatSection from './page-sections/State/State'
-// import Testimonials from './page-sections/Testimonials/Testimonials'
-
-// function App() {
-
-//   return (
-//     <>
-//        <Navbar></Navbar>
-//        <HeroSection></HeroSection>
-//        <Clients></Clients>
-//        <ServiceSection></ServiceSection>
-//        <ProcessSection></ProcessSection>
-//        <About></About>
-//        <StatSection></StatSection>
-//        <Testimonials></Testimonials>
-//        <Footer></Footer>
-//     </>
-//   )
-// }
-
-// export default App
+ 
