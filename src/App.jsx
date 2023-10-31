18640967a17641248bf4e94a9b63ad88540fc769
Abu Suraj <abusurajbd@gmail.com>
2023-10-31 19:26:42 +0600

    implement carousel

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar/Navbar'
 import ServiceSection from './components/Our-Services/Services'
 import ProcessSection from './components/Process-Section/ProcessSection'
 import StatSection from './components/State/State'
+import Testimonials from './components/Testimonials/Testimonials'
 
 function App() { 
 
@@ -19,6 +20,7 @@ function App() {
        <ProcessSection></ProcessSection>
        <About></About>
        <StatSection></StatSection>
+       <Testimonials></Testimonials>
        <Footer></Footer>
     </>
   )
